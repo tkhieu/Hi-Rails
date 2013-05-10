159cc51be90f7946846033b50f66b4c95bf8605f
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-11 00:28:46 +0700

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiRails::Application.config.secret_token = '84e5c338476e4ff94f06acccbefcbf30db440044d2df359ec88f3e5360f2a9e87f1fbb4ca59dd7f0c96a54e5920f69211f6fd81c1bfecfe01c1c28b2277203bb'
